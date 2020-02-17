f6a50e11deb9e1b5d8cee30d68bb3f605743e768
jinsu4755 <parkjinsu4755@daum.net>
2020-02-18 04:34:59 +0900

    jpa study

---
+++ Wordata_JPA/src/main/java/com/wordata/test/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.wordata.test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wordata.test.domain.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+}
